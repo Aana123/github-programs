ee7c8c29c238a9356f056a4028c056c6caf2a039
aana123 <aanapandey05@gmail.com>
2021-10-03 22:10:47 +0530

    my first commit

---
+++ for loop.py
@@ -0,0 +1,27 @@
+lst = ["aana", "likes", "tomatoes"]
+
+for x in range(len(lst)):
+    print(x)
+
+for x in range(len(lst)):
+    print(lst[x])
+
+for x in range(2,-1,-1):
+    print(x)
+
+for x in range(len(lst)):
+    print(len(lst)-x-1)
+
+for x in range(len(lst)):
+    print(lst[x])
+
+
+
+# x   printed
+# 0     2
+# 1     1
+# 2     0
+
+
+# for x in range(len(lst)):
+#     print(lst[x])   
